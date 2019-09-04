15165e2d69bf5e636bff3c1618a17fee33190358
Vinnny Merugumala <vinnnymerugumala@Vinnnys-MacBook-Pro.local>
2019-09-04 11:32:20 -0500

    styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,14 +7,22 @@ import "../styles/dashboard.css";
 
 export default function Dashboard() {
   return (
-    <>
-      <h1> Welcome User!</h1>
+    <div>
+      <h1 id="welcome"> Welcome User!</h1>
       <main className="dashboard">
+        <div className="nav-container-2">
+          <span className="nav-triangle-2"></span>
+          <nav>Fantasy Football Legacy</nav>
+        </div>
         <LegacyTeamList />
         <Players />
-        <News />
+        <body className="bigbody">
+          <div className="nfl-pic">
+            <News />
+          </div>
+        </body>
         <SideMenu />
       </main>
-    </>
+    </div>
   );
 }
