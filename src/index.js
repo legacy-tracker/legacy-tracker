517dda9c446fbfdcb05dc7d365b1e9cbaf58f365
Vinnny Merugumala <vinnnymerugumala@Vinnnys-MacBook-Pro.local>
2019-09-04 09:10:24 -0500

    idk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
+
   document.getElementById("root")
 );
 
