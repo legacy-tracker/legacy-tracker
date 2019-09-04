664474a84200d7e77ad95e88768c2e614392068e
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-09-04 16:26:31 -0500

    fixed package.json

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -32,29 +32,6 @@ export class SignIn extends React.Component {
         console.log(err);
       });
   }
-<<<<<<< HEAD
-  console.log(email, password);
-  return (
-    <div id="background">
-      <div className="register-card">
-        <h1>Sign In</h1>
-        <h2 className="register-text">Username</h2>
-        <input
-          className="register-input"
-          placeholder="username"
-          onChange={handleEmail}
-        />
-        <h2 className="register-text">Password</h2>
-        <input
-          type="password"
-          className="register-input"
-          placeholder="password"
-          onChange={handlePassword}
-        />
-        <Link to="/dashboard">
-          <button className="register-btn">Submit</button>
-        </Link>
-=======
   render() {
     if (this.props.redirect === true) {
       return <Redirect to="/dashboard" />;
@@ -81,7 +58,6 @@ export class SignIn extends React.Component {
             Submit
           </button>
         </div>
->>>>>>> master
       </div>
     );
   }
