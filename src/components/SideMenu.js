2c982b2cb8f58813f40ba4f6aa679f8be546ea15
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-09-05 14:40:51 -0500

    connected modal to logos

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../styles/dashboard.css";
+import "../styles/modal.scss";
 import MakeTeam from "./MakeTeam";
 
 export default class SideMenu extends Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false
-    };
-  }
+  state = { show: false };
 
   showModal = () => {
     this.setState({ show: true });
@@ -25,15 +21,12 @@ export default class SideMenu extends Component {
         <div className="create-team-card">
           <h4 className="card-header">FANTASY FOOTBALL LEGACY</h4>
           <h5 className="card-subheader">CREATE YOUR TEAM</h5>
-          <Modal show={this.state.show} handleClose={this.hideModal}>
-            <p>Modal</p>
-            <p>Data</p>
-          </Modal>
+          <Modal show={this.state.show} handleClose={this.hideModal} />
           <div className="challenge-btn-content">
             <button
               type="button"
               className="challenge-btns"
-              onclick={this.showModal}
+              onClick={this.showModal}
             >
               CREATE A TEAM
             </button>
@@ -51,7 +44,13 @@ const Modal = ({ handleClose, show, children }) => {
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        {children}
+        <h1>FANTASY FOOTBALL LEGACY</h1>
+        <h2>Enter your team name:</h2>
+        <MakeTeam />
+        <Link to="/logos">
+          <button>submit</button>
+        </Link>
+
         <button onClick={handleClose}>close</button>
       </section>
     </div>
