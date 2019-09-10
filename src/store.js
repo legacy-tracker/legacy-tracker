5eb80473ce2655a59c7b683033429efec5d355e2
Vinnny Merugumala <vinnnymerugumala@Vinnnys-MacBook-Pro.local>
2019-09-10 16:05:47 -0500

    issues

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise-middleware";
 import authReducer from "./ducks/authReducer";
 import legacyTeamReducer from "./ducks/legacyTeamReducer";
+import playersReducer from "./ducks/playersReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  legacy: legacyTeamReducer
+  legacy: legacyTeamReducer,
+  players: playersReducer
 });
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
