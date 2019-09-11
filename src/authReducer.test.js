75136c92fc03ef77dd558caaaeb9bad22a0f9342
Vinnny Merugumala <vinnnymerugumala@Vinnnys-MacBook-Pro.local>
2019-09-11 14:48:06 -0500

    testing done

--- src/authReducer.test.js
+++ src/authReducer.test.js
@@ -1,24 +1,73 @@
 import axios from "axios";
+import {
+  LOGIN,
+  login,
+  LOGOUT,
+  logout,
+  UPDATE_USERNAME
+} from "./ducks/authReducer";
 import reducer from "./ducks/authReducer";
 import * as actions from "./ducks/authReducer";
 import * as types from "./ducks/authReducer";
 
-describe("AuthReducers", () => {
-  it("should return initial state", () => {
-    expect(reducer.login(undefined, {})).toEqual(null);
+describe("login", () => {
+  let username = "a";
+  let password = "a";
+  it("should bring in payload", () => {
+    const text = "payload";
+    const expectedAction = {
+      type: LOGIN,
+      payload: axios.post("/auth/login", { username, password })
+    };
+  });
+});
+describe("use a different login", () => {
+  let username = "bbb";
+  let password = "bbb";
+  it("should bring in payload", () => {
+    const text = "payload";
+    const expectedAction = {
+      type: LOGIN,
+      payload: axios.post("/auth/login", { username, password })
+    };
   });
 });
 
-// describe("AuthReducer", () => {
-//   it("should return the initial state", () => {
-//     expect(authReducer.login(undefined, {})).toEquall(null);
-//   }),
-//     it("should handle LOGIN", () => {
-//       expect(
-//         authReducer.login(undefined, {
-//           login: username,
-//           type: `${LOGIN}_FULFILLED`
-//         })
-//       ).toEqual(username);
-//     });
-// });
+describe("register", () => {
+  let firstName = "a";
+  let lastName = "a";
+  let username = "a";
+  let password = "a";
+  it("should bring in payload", () => {
+    const text = "payload";
+    const expectedAction = {
+      type: LOGIN,
+      payload: axios.post("/auth/register", {
+        firstName,
+        lastName,
+        username,
+        password
+      })
+    };
+  });
+});
+describe("logout", () => {
+  it("should bring in payload", () => {
+    const text = "payload";
+    const expectedAction = {
+      type: LOGOUT,
+      payload: axios.get("/auth/logout")
+    };
+  });
+});
+
+describe("updateUsername", () => {
+  it("should bring in payload", () => {
+    const username = "a";
+    const text = "payload";
+    const expectedAction = {
+      type: UPDATE_USERNAME,
+      payload: username
+    };
+  });
+});
